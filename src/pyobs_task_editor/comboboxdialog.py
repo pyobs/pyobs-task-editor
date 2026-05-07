1a0000611669833f5f3e4ee1bfc652ae0a203f37
Tim-Oliver Husser <thusser@uni-goettingen.de>
2026-05-07 13:43:09 +0200

    working on merits/constraints

---
+++ src/pyobs_task_editor/comboboxdialog.py
@@ -0,0 +1,18 @@
+from PySide6 import QtWidgets
+
+
+class ComboBoxDialog(QtWidgets.QDialog):
+    def __init__(self, title: str, options: list[str]) -> None:
+        super().__init__()
+        self.setWindowTitle(title)
+        self.setLayout(QtWidgets.QHBoxLayout())
+        self.combo_box = QtWidgets.QComboBox()
+        self.combo_box.addItems(options)
+        self.layout().addWidget(self.combo_box)
+        self.button = QtWidgets.QPushButton("OK")
+        self.layout().addWidget(self.button)
+        self.button.clicked.connect(self.accept)
+
+    @property
+    def option(self) -> str:
+        return self.combo_box.currentText()
--- src/pyobs_task_editor/listwidget.py
+++ src/pyobs_task_editor/listwidget.py
@@ -1,7 +1,11 @@
+from typing import cast
+
 from PySide6 import QtWidgets, QtCore
 import qtawesome as qa
 from pyobs.robotic import Task
 import inspect
+
+from pyobs_task_editor.comboboxdialog import ComboBoxDialog
 
 
 class ListWidget(QtWidgets.QGroupBox):
@@ -24,6 +28,7 @@ class ListWidget(QtWidgets.QGroupBox):
         list_frame.setLayout(list_layout)
 
         self.list_widget = QtWidgets.QListWidget()
+        self.list_widget.currentItemChanged.connect(self._item_selected)
         list_layout.addWidget(self.list_widget)
 
         buttons_layout = QtWidgets.QHBoxLayout()
@@ -43,20 +48,50 @@ class ListWidget(QtWidgets.QGroupBox):
         self.button_remove.clicked.connect(self.remove_item)
         buttons_layout.addWidget(self.button_remove)
 
-        edit_group = QtWidgets.QGroupBox()
-        splitter.addWidget(edit_group)
+        self.edit_group = QtWidgets.QGroupBox()
+        self.edit_group.setLayout(QtWidgets.QFormLayout())
+        splitter.addWidget(self.edit_group)
+
+        splitter.setSizes([splitter.width() // 2, splitter.width() // 2])
 
     @QtCore.Slot()
     def add_item(self):
         import pyobs.robotic.scheduler.constraints
 
-        items = [name for name, obj in inspect.getmembers(pyobs.robotic.scheduler.constraints) if inspect.isclass(obj)]
-        print(items)
+        existing = [self.list_widget.item(row).text() for row in range(self.list_widget.count())]
+        options = [
+            name
+            for name, obj in inspect.getmembers(pyobs.robotic.scheduler.constraints)
+            if inspect.isclass(obj) and name not in existing
+        ]
 
-        item = QtWidgets.QListWidgetItem()
-        item.setText("Add item")
-        self.list_widget.addItem(item)
+        dialog = ComboBoxDialog("Select type", options)
+        if dialog.exec_() == QtWidgets.QDialog.DialogCode.Accepted:
+            item = QtWidgets.QListWidgetItem()
+            item.setText(dialog.option)
+            self.list_widget.addItem(item)
 
     @QtCore.Slot()
     def remove_item(self):
         pass
+
+    @QtCore.Slot(QtWidgets.QListWidgetItem)
+    def _item_selected(self, item):
+        import pyobs.robotic.scheduler.constraints
+
+        print(item.text())
+        klass = getattr(pyobs.robotic.scheduler.constraints, item.text())
+        print(klass)
+        print(klass.model_fields)
+
+        layout = cast(QtWidgets.QFormLayout, self.edit_group.layout())
+        for i in reversed(range(layout.count())):
+            layout.itemAt(i).widget().setParent(None)
+
+        for name, info in klass.model_fields.items():
+            if info.annotation is float:
+                widget = QtWidgets.QDoubleSpinBox()
+            else:
+                widget = QtWidgets.QLineEdit()
+            layout.addRow(name, widget)
+            print(info.annotation)
